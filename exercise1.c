a7cf52d85752f4944d6f37fd85abd67abf95da6d
niknursyarwani <102133391+niknursyarwani@users.noreply.github.com>
2022-05-31 01:47:28 +0800

    Update exercise1.c

--- exercise1.c
+++ exercise1.c
@@ -3,15 +3,41 @@
 
 //pre-processor section 
 #include<stdio.h>
+ 
+// A recursive binary search function. It returns
+// location of x in given array arr[l..r] is present,
+// otherwise -1
 
 //global variable section 
 
+int binarySearch(int arr[], int l, int r, int x)
+{
+    if (r >= l) 
+    {
+        int mid = l + (r - l) / 2;
+ 
+        if (arr[mid] == x)
+            return mid;
+ 
+        if (arr[mid] > x)
+            return binarySearch(arr, l, mid - 1, x);
+ 
+        return binarySearch(arr, mid + 1, r, x);
+    }
 
-//main function section 
-int main(){
-    //write here your program
-    
-
+    return 6;
 }
+ 
+int main(void)
+{
+    int arr[] = { 10, 20, 80, 30, 60, 50, 110, 100, 130, 170};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    int x = 110;
+    int result = binarySearch(arr, 0, n - 1, x);
+    (result == -6)
+       ? printf("Element is not present in array")
+       : printf("Element with value 110 is present at index %d", result);
+        
+    return 0;
 
-//user-defined section 
+    
